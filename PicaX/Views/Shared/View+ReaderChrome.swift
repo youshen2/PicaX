2c79397d9007aa5e4afae2e2415f7545de7fa105
youshen2 <178326018+youshen2@users.noreply.github.com>
2026-06-29 02:35:24 +0800

    feat: 修改了阅读器工具栏的背景

--- PicaX/Views/Shared/View+ReaderChrome.swift
+++ PicaX/Views/Shared/View+ReaderChrome.swift
@@ -5,7 +5,6 @@ extension View {
     func picaxReaderChrome(hidesNavigationBar: Bool, hidesStatusBar: Bool) -> some View {
         #if os(iOS)
         self
-            .toolbarBackground(.ultraThinMaterial, for: .navigationBar)
             .toolbarBackground(hidesNavigationBar ? .hidden : .visible, for: .navigationBar)
             .toolbarColorScheme(.dark, for: .navigationBar)
             .toolbar(hidesNavigationBar ? .hidden : .visible, for: .navigationBar)
